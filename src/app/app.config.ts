c4898132814a5746d5ef5f2b33d4df4f1a861c5f
Alf123Mejia <ja.c5pac22@gmail.com>
2025-11-22 14:57:27 -0600

    Agregar el cambio de coma a punto en las cantidades

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,21 @@
-// src/app/app.config.ts
-
-// NUEVO: Importaciones necesarias para el idioma
 import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { registerLocaleData } from '@angular/common';
-import localeEs from '@angular/common/locales/es'; // Importamos el paquete de idioma español
+// CAMBIO: Importamos 'es-SV'
+import localeEsSV from '@angular/common/locales/es-SV';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
-// NUEVO: Registramos el idioma español en Angular
-registerLocaleData(localeEs);
+// CAMBIO: Registramos 'es-SV'
+registerLocaleData(localeEsSV);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(), // <-- ASEGÚRATE DE AÑADIR ESTA LÍNEA
-    // NUEVO: Le decimos a Angular que use 'es' (español) como el idioma por defecto para toda la app
-    { provide: LOCALE_ID, useValue: 'es' }
+    provideHttpClient(),
+    // CAMBIO: Usamos 'es-SV'
+    { provide: LOCALE_ID, useValue: 'es-SV' }
   ]
 };
